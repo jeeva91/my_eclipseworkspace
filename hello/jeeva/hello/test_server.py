3ac750de3d971c70ea283869b2056a17eb8de2a9
jeeva91 <jee1199@gmail.com>
2017-04-02 15:37:19 -0400

    added test server and client for the main TCP

--- hello/jeeva/hello/test_server.py
+++ hello/jeeva/hello/test_server.py
@@ -16,4 +16,17 @@ from jeeva.hello import Received_Processor
 server_send=Queue(0)
 server_receive=Queue(0)
 
-server_socket=My_TCP.My_TCP("127.0.0.1",5007,"client")
+server_socket=My_TCP.My_TCP("155.246.109.143",5005,"server")
+
+
+server_sender=Sender_Thread.Sender_Thread(server_send,server_socket.conn)
+server_receiver=Receiver_Thread.Receiver_Thread(server_receive,server_socket.conn)
+
+server_send_processor= Send_Processor.Send_Processor(server_send)
+server_receive_processor=Received_Processor.Received_Processor(server_receive,con_type="server")
+
+server_sender.start()
+server_receiver.start()
+
+server_send_processor.start()
+server_receive_processor.start()
